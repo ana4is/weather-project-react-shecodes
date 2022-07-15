c71b54e1da8b53058e6a3d9aeee88bcbe0e90e9f
ana4is <anaisflechier@gmail.com>
2022-07-15 15:03:27 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+        <Weather />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Weather (){
+    return (
+      <div className="Weather">
+        <button type="button" class="btn btn-warning">
+          Warning
+        </button>
+      </div>
+    );
+}
