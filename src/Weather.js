009ff9f3262b1499a52458c671dc9ff051b342f4
ana4is <anaisflechier@gmail.com>
2022-07-31 21:34:04 +0200

    Added useState

--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showWeatherDescritption(response) {
+    console.log(response.data);
+    setSearch(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      precipitations: response.data.main.pressure,
+      description: response.data.weather.description,
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+    });
+  }
+
+  function showCity(event) {
+    setCity(event.target.value);
+  }
+
   function searchEngine() {
     let apiKey = "2f9f2231f1186119c9fd6df5c383a288";
     let unit = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(showWeatherDescritption);
   }
-  return (
-    <div className="Weather">
-      <form>
-        <input type="search" placeholder="Type a city..." />
-        <input type="submit" value="search" />
-      </form>
-      <h1>SEVILLA</h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Temperature: 20 C</div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitations :</li>
-              <li>Wind:</li>
-              <li>Humidity</li>
-            </ul>
+  if (search) {
+    return (
+      <div className="Weather">
+        <form onSubmit={searchEngine}>
+          <div className="row">
+            <span className="col-9">
+          <input type="search" placeholder="Type a city..." />
+          </span>
+          <input type="submit" value="search" />
+        </form>
+        <h1>SEVILLA</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">Temperature: 20 C</div>
+            <div className="col-6">
+              <ul>
+                <li>Precipitations :</li>
+                <li>Wind:</li>
+                <li>Humidity</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <p>new react</p>
+        <button type="button" class="btn btn-info">
+          Info
+        </button>
       </div>
-      <p>new react</p>
-      <button type="button" class="btn btn-info">
-        Info
-      </button>
-    </div>
-  );
+    );
+  } else {
+    searchEngine();
+    return "Loading...";
+  }
 }
